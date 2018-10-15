01f2e4391239a0086c77b002b728b574553d89ef
daljeet1995 <ds025272@gmail.com>
2018-10-15 19:26:13 +0530

    First commit

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,12 +5,12 @@
   class Home extends CI_Controller {
 
   	public function __construct(){
-  		
+       parent ::__construct();
   	}
 
       public function index()
 	{
-		echo('Hello');
+		$this->load->view('home');
 	}
 
 	function h1() {
---
+++ application/views/home.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LMS-HOME</title>
+</head>
+<body>
+   <h1>111Hello</h1>
+</body>
+</html>
